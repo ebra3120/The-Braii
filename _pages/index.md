f13758e9624a6d3498f006d8dfdab3f3aaa73e5b
Saahil Ebrahim <ebra3120@mylaurier.ca>
2021-04-16 07:25:14 +0200

    new changes 123

--- _pages/index.md
+++
@@ -1,3 +0,0 @@
-# Home
-
-Index or home page here.
--- _partials/Head.js
+++
@@ -1,8 +0,0 @@
-module.exports = class{
-    render(){
-        return `
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" />
-<link rel="stylesheet" href="css/main.css" />
-        `;
-    }
-}
